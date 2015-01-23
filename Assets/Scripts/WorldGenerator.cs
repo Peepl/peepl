e1c10437c585790a828bf53e4eee77533f54deb3
Anssi Penttilä <anssi.penttila@roidu.com>
2015-01-23 23:59:19 +0200

    placeholder assets and start of world generator

---
+++ Assets/Scripts/WorldGenerator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldGenerator : MonoBehaviour {
+
+	public int sizeX;
+	public int sizeY;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Generate() {
+
+
+	}
+}
