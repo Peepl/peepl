adc5abdef5b7985122687e76eda580e7ccab2011
Henri Sarasvirta <henri.sarasvirta@gmail.com>
2015-01-25 00:03:24 +0200

    obelisk death.

--- Assets/Scripts/Obelisk.cs
+++ Assets/Scripts/Obelisk.cs
@@ -7,6 +7,9 @@ public class Obelisk : MonoBehaviour {
 
     private bool Friendly;
     private bool Active;
+	private int rotTarget = 0;
+	private bool rotate = false;
+	private int currot = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -31,6 +34,7 @@ public class Obelisk : MonoBehaviour {
                 GameObject swarm = GameObject.Find("Swarm");
                 SwarmAI swarmai = swarm.GetComponent<SwarmAI>();
                 swarm.GetComponent<SwarmAI>().Bless();
+				transform.Find("bad").gameObject.SetActive(true);
 
                 Debug.Log("Friendly obelisk triggered");
             }
@@ -40,10 +44,20 @@ public class Obelisk : MonoBehaviour {
                 GameObject swarm = GameObject.Find("Swarm");
                 swarm.GetComponent<SwarmAI>().KillInRadius(transform.position, 25.0f, 0.25f);
 
+				rotTarget += 360;
+				rotate = true;
+
+				if(swarm.GetComponent<SwarmAI>().TribeColor == GameColor.Blue)
+					transform.Find("obelisk").Find("obeliski").Find("blue").gameObject.SetActive(true);
+				else
+					transform.Find("obelisk").Find("obeliski").Find("red").gameObject.SetActive(true);
+
                 Debug.Log("Unfriendly obelisk triggered");
             }
         }
     }
+
+
 
     void OnTriggerExit(Collider collider)
     {
@@ -56,6 +70,17 @@ public class Obelisk : MonoBehaviour {
    
     // Update is called once per frame
 	void Update () {
-
+		if(rotate)
+		{
+			currot += 20;
+			var t = this.transform.Find("obelisk").Find("obeliski");
+			t.Rotate(new Vector3(0,20,0));
+			if(currot > rotTarget)
+			{
+				transform.Find("obelisk").Find("obeliski").Find("blue").gameObject.SetActive(false);
+				transform.Find("obelisk").Find("obeliski").Find("red").gameObject.SetActive(false);
+				t.rotation = Quaternion.Euler(new Vector3(0,rotTarget,0));
+			}
+		}
 	}
 }
