8478b8459baca7074a45fe8e71bd90f8ad3f56a0
daemou <jotain@invalid>
2015-01-25 13:40:30 +0200

    Event manager

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventManager : MonoBehaviour {
+
+    public enum GameColor { White, Blue, Black, Red, Green };
+    public enum EventType { Temple, Obelisk, Orb, Cavern, Oasis };
+
+    public GameColor TribeColor;
+
+	// Use this for initialization
+	void Start () {
+        TribeColor = GameColor.White;
+	}
+
+	
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
